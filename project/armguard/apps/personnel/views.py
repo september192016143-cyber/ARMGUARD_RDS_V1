fa3647418325770f0d4a9b1cf85840fe22ab367f
Stealth3535 <september192016143@gmail.com>
2026-03-11 00:09:48 +0800

    fix: set personnel list paginate_by to 10

--- project/armguard/apps/personnel/views.py
+++ project/armguard/apps/personnel/views.py
@@ -81,7 +81,7 @@ class PersonnelListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
 	model = Personnel
 	template_name = "personnel/personnel_list.html"
 	context_object_name = "personnel_list"
-	paginate_by = 9
+	paginate_by = 10
 	ordering = ['rank', 'last_name']
 
 	def get_queryset(self):
